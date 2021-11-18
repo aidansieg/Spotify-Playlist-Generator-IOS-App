3cdcc4005cbf39611d7b7f47041fab0338b90426
Aidansieg <aidansieg@gmail.com>
2021-11-18 10:28:16 -0500

    Set up local repo

---
+++ bingbong/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainMenuViewController.swift
+//  bingbong
+//
+//  Created by Aidan Siegel on 11/9/21.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+}
---
+++ bingbong/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  bingbong
+//
+//  Created by Aidan Siegel on 11/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //@IBOutlet weak var segueTextField: UITextField!
+
+    @IBAction func login(_ sender: UIButton) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+    }
+    
+}
+
