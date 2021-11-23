67e59d2e2d24e4167d03e0ee0410f57d2756dbc6
rafarrel <rfarrel2@uvm.edu>
2021-11-23 15:49:00 -0500

    Added basic Spotify login logic to login button in first screen

--- Playlistify/AppDelegate.swift
+++ Playlistify/AppDelegate.swift
@@ -1,24 +1,37 @@
-//
-//  AppDelegate.swift
-//  bingbong
-//
-//  Created by Aidan Siegel on 11/9/21.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // Spotify App Delegate settings
+    var window: UIWindow?
+    lazy var rootViewController = ViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        rootViewController.sessionManager.application(app, open: url, options: options)
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        if (rootViewController.appRemote.isConnected) {
+            rootViewController.appRemote.disconnect()
+        }
+    }
 
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        if let _ = rootViewController.appRemote.connectionParameters.accessToken {
+            rootViewController.appRemote.connect()
+        }
+    }
+    
+    // Default App Delegate settings
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -31,6 +44,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
