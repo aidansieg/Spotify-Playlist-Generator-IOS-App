26ab53e8e5a29f1931f5b23df3c8b868beab9a23
rafarrel <rfarrel2@uvm.edu>
2021-11-24 15:21:58 -0500

    Updated redirect url

--- Playlistify/LoginViewController.swift
+++ Playlistify/LoginViewController.swift
@@ -50,7 +50,7 @@ class LoginViewController: UIViewController, SPTSessionManagerDelegate, SPTAppRe
     
     // Spotify authorization
     private let SpotifyClientID    = spotifyClientID
-    private let SpotifyRedirectURI = URL(string: "Playlistify://")!
+    private let SpotifyRedirectURI = URL(string: "Playlistify://spotify-login-callback")!
 
     // Spotify configuration for connection
     lazy var configuration: SPTConfiguration = {
