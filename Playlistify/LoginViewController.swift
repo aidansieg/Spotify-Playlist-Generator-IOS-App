bc95d1e1ac8ecec684d2c9aba4a22d1a8143c281
rafarrel <rfarrel2@uvm.edu>
2021-11-24 17:06:01 -0500

    Test manually adding client secret

--- Playlistify/LoginViewController.swift
+++ Playlistify/LoginViewController.swift
@@ -61,8 +61,8 @@ class LoginViewController: UIViewController, SPTSessionManagerDelegate, SPTAppRe
 
         // URLs containing the secret to exchange for an access token
         // KEEP THESE COMMENTED OUT FOR REFERENCE
-        configuration.tokenSwapURL    = URL(string: "http://localhost:1234/swap")
-        configuration.tokenRefreshURL = URL(string: "http://localhost:1234/refresh")
+        //configuration.tokenSwapURL    = URL(string: "http://localhost:1234/swap")
+        //configuration.tokenRefreshURL = URL(string: "http://localhost:1234/refresh")
         return configuration
     }()
     
@@ -75,6 +75,7 @@ class LoginViewController: UIViewController, SPTSessionManagerDelegate, SPTAppRe
     // Spotify app remote connection
     lazy var appRemote: SPTAppRemote = {
         let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.connectionParameters.accessToken = "c815832c3d8b4311a57f5f132b256e9e"
         appRemote.delegate = self
         return appRemote
     }()
