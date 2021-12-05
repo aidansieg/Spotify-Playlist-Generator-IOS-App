14c41dba74d31d1d5fb483d84d680c4a0acb04f4
rafarrel <rfarrel2@uvm.edu>
2021-12-04 19:37:18 -0500

    Added validation check/invalid pop up alert functionality for year range

--- Playlistify/YearViewController.swift
+++ Playlistify/YearViewController.swift
@@ -18,6 +18,20 @@ class YearViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    // Create-playlist button
+    @IBAction func createPlaylist(_ sender: UIButton) {
+        if let startYear = playlistStartYear.text, let endYear = playlistEndYear.text {
+            // Title length check
+            if self.validateYears(startYear, endYear) {
+                self.performSegue(withIdentifier: "year-page-to-playlist-page", sender: nil)
+            } else {
+                self.presentAlertController(title: "Invalid Playlist Year Range",
+                                            message: "Playlist year range should be between 1920-2021 and be valid years.",
+                                            buttonTitle: "OK")
+            }
+        }
+    }
+    
     // Segues from the create a playlist year page
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch segue.identifier {
@@ -31,6 +45,31 @@ class YearViewController: UIViewController {
         default:
             preconditionFailure("Unexpected segue identifier")
             break
+        }
+    }
+    
+    //********************************************* Helper functions *********************************************//
+    
+    // Used to make sure playlist year is valid
+    func validateYears(_ startYear: String, _ endYear: String) -> Bool {
+        if (Int(startYear) == nil || Int(endYear) == nil) {
+            return false
+        } else if (Int(endYear)! < Int(startYear)!) {
+            return false
+        } else if (Int(startYear)! < 1920 || Int(startYear)! > 2021) || Int(endYear)! < 1920 || Int(endYear)! > 2021 {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func presentAlertController(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let action     = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
+            
+            controller.addAction(action)
+            self.present(controller, animated: true)
         }
     }
 }
